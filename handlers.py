df0dde814207a8a1a35b6fe74aa39dfc8f1aa1da
Kayque <kayques.es@gmail.com>
2025-06-30 12:42:32 -0300

    try

--- handlers.py
+++ handlers.py
@@ -641,7 +641,7 @@ async def confirmacao(update, context):
     if data == "confirmar_salvar":
         utils.salvar_no_banco(context.user_data) 
         utils.export_data_to_drive()
-        utils.salvar_demandas_no_banco(user_data, user_data.get("demandas", []))
+        utils.salvar_demandas_no_banco(context.user_data, context.user_data.get("demandas", []))
         await query.edit_message_text(
             "🎉 Dados salvos com sucesso no banco de dados e nos arquivos Excel do Google Drive! Muito obrigado pelo seu registro.", 
             parse_mode=ParseMode.HTML
